d42055bd062bdfdeb8504886a6067b5b3807e434
Axel Nieto <axelnieto616@gmail.com>
2021-12-04 18:38:33 -0300

    primer commit

---
+++ src/components/CoinRow.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const CoinRow = ({coin,index}) => {
+    return(
+        <tr>
+            <td>{index}</td>
+            <td>
+                <img src={coin.image} alt={coin.name} className='me-4' style={{width:'3%'}}/>
+                <span>{coin.name}</span>
+                <span className='ms-3 text-uppercase text-muted'>{coin.symbol}</span>
+            </td>
+            <td>{coin.current_price}</td>
+            <td className={coin.price_change_percentage_24h>=0? 'text-success':'text-danger'}>
+                {coin.price_change_percentage_24h}
+            </td>
+            <td>{coin.total_volume}</td>
+        </tr>
+    )
+}
+
+export default CoinRow;
